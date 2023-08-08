033fe645650f54fa5bb5134a7c767c8c7c8e616e
defy <8841170+defydef@users.noreply.github.com>
2023-08-08 23:13:42 +0700

    Use callback function as argument in setLikes so that likes is always updated based on the latest likes state

--- src/App.js
+++ src/App.js
@@ -68,13 +68,23 @@ function TabContent({ item }) {
   console.log("RENDER");
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleTripleInc() {
+    handleInc();
+    handleInc();
+    handleInc();
   }
 
   function handleUndo() {
     setShowDetails(true);
     setLikes(0);
     console.log(likes);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -90,13 +100,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
         <button onClick={handleUndo}>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
